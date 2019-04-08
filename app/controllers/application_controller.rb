e8b74bc15d541a3077d3739e07f48d0107ae3a80
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-04-08 17:11:17 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,8 +43,9 @@ class ApplicationController < Sinatra::Base
 
 
   patch '/articles/:id' do
-    @article = Article.find_by(params[:id])
     binding.pry
+    @article = Article.find_by(params[:id])
+    # binding.pry
     @article.title = params[:title]
     @article.content = params[:content]
     @article.save
